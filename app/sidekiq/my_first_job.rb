7660dbbb0f0682e0836b53ea0c1bbe2ca8b5d8b1
Suhani Kaushal <suhanikaushal57@gmail.com>
2026-08-11 20:05:52 +0530

    add job

--- app/sidekiq/my_first_job.rb
+++ app/sidekiq/my_first_job.rb
@@ -1,7 +1,12 @@
 class MyFirstJob
   include Sidekiq::Job
 
-  def perform(val)
+  def perform(val, *args)
     puts "My first Sidekiq job is running!"
+    performs(val)
+  end
+
+  def performs(val)
+    puts "#{val}(method) Sidekiq job is running!"
   end
 end
